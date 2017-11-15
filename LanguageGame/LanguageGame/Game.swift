29e6dec50d559ed3cac4a2751253cb169785281e
David Lashkhi <lashkhi@me.com>
2017-11-15 23:06:16 +0100

    implementing draft design

--- LanguageGame/LanguageGame/Game.swift
+++ LanguageGame/LanguageGame/Game.swift
@@ -9,31 +9,39 @@
 import Foundation
 
 final class Game {
-    private var words: [Word]?
+    private var originalWords: [Word]?
+    var words = [Word]()
     
     init(words: [Word]) {
-        self.words = words
+        self.originalWords = words
     }
     
     func createWordsCombination() {
-        var newWords = [Word]()
-        for (index, word) in words!.enumerated() {
+        for (index, word) in originalWords!.enumerated() {
             var newWord = word
             if index % 4 == 0 {
                 let randomWord = chooseRandomWordAndCompare(word: word)
                 newWord = Word(english: word.english, spanish: randomWord.spanish, isWrongCombination: true)
             }
-            newWords.append(newWord)
+            words.append(newWord)
         }
     }
     
     func chooseRandomWordAndCompare(word: Word) -> Word {
-        let randomNumber = Int(arc4random_uniform(UInt32(words!.count)))
-        var randomWord = words![randomNumber]
+        let randomNumber = Int(arc4random_uniform(UInt32(originalWords!.count)))
+        var randomWord = originalWords![randomNumber]
         if randomWord.spanish == word.spanish {
             randomWord = chooseRandomWordAndCompare(word: word)
         }
         return randomWord
+    }
+    
+    func loadNextWords() -> Word? {
+        if words.isEmpty  {
+            return nil
+        }
+        words.remove(at: 0)
+        return words.first
     }
     
 }
--- LanguageGame/LanguageGame/ViewController.swift
+++ LanguageGame/LanguageGame/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var english: UILabel!
+    @IBOutlet weak var translation: UILabel!
+    @IBOutlet weak var wrongAnswerCounter: UILabel!
+    
+    var game: Game?
+    var words: [Word]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let defaultGame = GameLoader.createDefault() {
+            start(game: defaultGame)
+            updateLabelsWith(word: words!.first!)
+        }
     }
+    
+    func start(game: Game) {
+        self.game = game
+        game.createWordsCombination()
+        words = game.words
+    }
+    
+    func checkCorrectness(correctButtonTapped: Bool) {
+        if let word = words?.first {
+            word.isWrongCombination
+        }
+    }
+    func updateLabelsWith(word: Word) {
+        english.text = word.english
+        translation.text = word.spanish
+    }
+    
+    func showNextWord() {
+        if let word = game?.loadNextWords() {
+            updateLabelsWith(word: word)
+        } else { print("Game finished") }
+    }
+    
 
+    @IBAction func wrongTap(_ sender: UIButton) {
+        buttonTapped(wrongButtonTapped: true)
+    }
+    
+    @IBAction func correctTap(_ sender: UIButton) {
+        buttonTapped(wrongButtonTapped: false)
+    }
+    
+    func buttonTapped(wrongButtonTapped: Bool) {
+        showNextWord()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
